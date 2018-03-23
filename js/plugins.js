8f47809bb6f7ff1eca2e7e146e8e4da0c0fb3ac4
sharef184 <sharifkhlief@gmail.com>
2018-03-23 17:40:51 +0100

    aa

---
+++ js/plugins.js
@@ -0,0 +1,85 @@
+//First Effect
+(function($) {
+  var s,
+  spanizeLetters = {
+    settings: {
+      letters: $('.effect'),
+    },
+    init: function() {
+      s = this.settings;
+      this.bindEvents();
+    },
+    bindEvents: function(){
+      s.letters.html(function (i, el) {
+        //spanizeLetters.joinChars();
+        var spanizer = $.trim(el).split("");
+        return '<span>' + spanizer.join('</span><span>') + '</span>';
+      });
+    },
+  };
+  spanizeLetters.init();
+})(jQuery);
+
+//Second Effect - Typing Effect
+(function() {
+  var app;
+
+  $(document).ready(function() {
+    return app.init();
+  });
+
+  app = {
+    text: "Hello, I'm Sharif Khlief a freelance developer based in Prague. I've always been drawn to the overlap between design and development. My skills are broad: front end to back end development. I enjoy each aspect, and love building a website from start to finish, I build rich web environments using the most sophisticated technologies available today for interactive and responsive websites, for clients all over the world.",
+    index: 0,
+    chars: 0,
+    speed: 100,
+    container: ".typing-effect .content",
+    init: function() {
+      this.chars = this.text.length;
+      return this.write();
+    },
+    write: function() {
+      $(this.container).append(this.text[this.index]);
+      if (this.index < this.chars) {
+        this.index++;
+        return window.setTimeout(function() {
+          return app.write();
+        }, this.speed);
+      }
+    }
+  };
+
+}).call(this);
+
+// Sticky Nav Bar
+$(document).ready(function() {
+		   	var stickyNavTop = $('#toggle').offset().top;
+		   	var stickyNav = function(){
+			    var scrollTop = $(window).scrollTop();
+
+			    if (scrollTop > stickyNavTop) { 
+			        $('#toggle').addClass('sticky');
+			    } else {
+			        $('#toggle').removeClass('sticky'); 
+			    }
+			};
+
+			stickyNav();
+	
+			$(window).scroll(function() {
+				stickyNav();
+			});
+		});
+
+// toggle Nav menu 
+$("#toggle").click(function() {
+  $(this).toggleClass("on");
+  $("nav").slideToggle(500);
+});
+
+$("nav ul li a").click(function() {
+	if ($("#toggle").hasClass("on") === true) {
+		$("nav").slideToggle(500);
+		$("#toggle").removeClass("on");
+	}
+});
